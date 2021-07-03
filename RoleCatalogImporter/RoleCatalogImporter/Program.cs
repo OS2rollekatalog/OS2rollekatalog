da5edfb30fa974a50b09e196dcecd53c11440e84
Brian Graversen <bsg@digital-identity.dk>
2021-07-03 20:25:56 +0200

    added TLS 1.2 support for importer

--- RoleCatalogImporter/RoleCatalogImporter/Program.cs
+++ RoleCatalogImporter/RoleCatalogImporter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace RoleCatalogImporter
     {
         static void Main(string[] args)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             Configuration.Configure();
         }
     }
